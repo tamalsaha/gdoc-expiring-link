461d5532278218e034f6c6a2ec0909fca4f3cb0c
Tamal Saha <tamal@appscode.com>
2022-05-28 05:49:17 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,25 @@
 module github.com/tamalsaha/gdoc-expiring-link
 
 go 1.18
+
+require (
+	golang.org/x/net v0.0.0-20220526153639-5463443f8c37
+	gomodules.xyz/gdrive-utils v0.0.3
+	google.golang.org/api v0.81.0
+)
+
+require (
+	cloud.google.com/go/compute v1.6.1 // indirect
+	github.com/gocarina/gocsv v0.0.0-20211020200912-82fc2684cc48 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
+	google.golang.org/grpc v1.46.2 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,3 +1,94 @@
+// Sample Go code for user authorization
+
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"golang.org/x/net/context"
+	_ "gomodules.xyz/gdrive-utils"
+	gdrive_utils "gomodules.xyz/gdrive-utils"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+)
+
+func handleError(err error, message string) {
+	if message == "" {
+		message = "Error making API call"
+	}
+	if err != nil {
+		log.Fatalf(message+": %v", err.Error())
+	}
+}
+
+// https://developers.google.com/youtube/v3/guides/working_with_channel_ids
+// Use channel id
+// Youtube Studio > Customization > Basic Info
+func channelsListByUsername(service *youtube.Service, parts []string, channelID string) {
+	call := service.Channels.List(parts)
+	call = call.Id(channelID)
+	response, err := call.Do()
+	handleError(err, "")
+
+	fmt.Println(fmt.Sprintf("This channel's ID is %s. Its title is '%s', "+
+		"and it has %d views.",
+		response.Items[0].Id,
+		response.Items[0].Snippet.Title,
+		response.Items[0].Statistics.ViewCount))
+}
+
+// https://developers.google.com/youtube/v3/getting-started#partial
+// parts vs fields
+// parts is top level section
+// fields are fields inside that section
+
+const channelID = "UCxObRDZ0DtaQe_cCP-dN-xg"
+
+func main() {
+	ctx := context.Background()
+
+	client, err := gdrive_utils.DefaultClient(".", youtube.YoutubeReadonlyScope)
+	handleError(err, "Error creating YouTube client")
+	service, err := youtube.NewService(ctx, option.WithHTTPClient(client))
+	handleError(err, "Error creating YouTube client")
+
+	// channelsListByUsername(service, strings.Split("snippet,contentDetails,statistics", ","), channelID)
+
+	ListPlaylists(service, channelID)
+	// ListPlaylistItems(service, "PLoiT1Gv2KR1gc4FN0f7w92RhAHTKbPotT")
+}
+
+// playlist
+// playlistItem
+// thumbnail
+// video
+
+// https://developers.google.com/youtube/v3/guides/implementation/playlists
+func ListPlaylists(service *youtube.Service, channelID string) ([]*youtube.Playlist, error) {
+	call := service.Playlists.List(strings.Split("snippet,contentDetails,status", ","))
+	// call = call.Fields("items(id,snippet(title,description,publishedAt,tags,thumbnails(high)),contentDetails,status)")
+	call = call.ChannelId(channelID)
+
+	var out []*youtube.Playlist
+	err := call.Pages(context.TODO(), func(resp *youtube.PlaylistListResponse) error {
+		out = append(out, resp.Items...)
+		return nil
+	})
+	return out, err
+}
+
+// https://developers.google.com/youtube/v3/docs/playlistItems/list
+func ListPlaylistItems(service *youtube.Service, playlistID string) ([]*youtube.PlaylistItem, error) {
+	call := service.PlaylistItems.List(strings.Split("snippet,contentDetails,status", ","))
+	call = call.Fields("items(snippet(title,description,position,thumbnails(high)),contentDetails,status)")
+	call = call.PlaylistId(playlistID)
+
+	var out []*youtube.PlaylistItem
+	err := call.Pages(context.Background(), func(resp *youtube.PlaylistItemListResponse) error {
+		out = append(out, resp.Items...)
+		return nil
+	})
+	return out, err
+}
