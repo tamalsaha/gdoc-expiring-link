5ae3493cdab0c3444c5004224c63539d1a302892
Tamal Saha <tamal@appscode.com>
2022-05-29 10:19:51 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"log"
 	"path"
-	"strings"
 	"time"
 
 	"github.com/gocarina/gocsv"
@@ -79,12 +78,12 @@ func main_() {
 
 	email := "tamal.saha@gmail.com"
 
-	folderId, err := GetFolderId(svcDrive, fileId, "candidates/"+email)
+	folderId, err := gdrive.GetFolderId(svcDrive, fileId, "candidates/"+email)
 	handleError(err, "Error finding parent folder id")
 	fmt.Println("parent folder id", folderId)
 
 	now := time.Now().UTC()
-	docId, err := CopyDoc(svcDrive, svcDocs, fileId, folderId, "Account Interview", map[string]string{
+	docId, err := gdrive.CopyDoc(svcDrive, svcDocs, fileId, folderId, "Account Interview", map[string]string{
 		"{{email}}":      email,
 		"{{start-time}}": now.Format(time.RFC3339),
 		"{{end-time}}":   now.Add(1 * time.Hour).Format(time.RFC3339),
@@ -98,15 +97,6 @@ func main_() {
 }
 
 const fileId = "16Ff6Lum3F6IeyAEy3P5Xy7R8CITIZRjdwnsRwBg9rD4"
-
-func AddPermission(svc *drive.Service, docId string, email string, role string) error {
-	_, err := svc.Permissions.Create(docId, &drive.Permission{
-		EmailAddress: email,
-		Role:         role,
-		Type:         "user",
-	}).Fields("id").Do()
-	return err
-}
 
 // https://developers.google.com/youtube/v3/getting-started#partial
 // parts vs fields
@@ -129,99 +119,6 @@ func RevokePermission(svc *drive.Service, docId string, email string) error {
 		}
 	}
 	return err
-}
-
-func FindParentFolderId(svc *drive.Service, configDocId string) (string, error) {
-	d, err := svc.Files.Get(configDocId).Fields("parents").Do()
-	if err != nil {
-		return "", err
-	}
-	return d.Parents[0], nil
-}
-
-func GetFolderId(svc *drive.Service, configDocId string, p string) (string, error) {
-	parentFolderId, err := FindParentFolderId(svc, configDocId)
-	if err != nil {
-		return "", errors.Wrap(err, "failed to detect root folder id")
-	}
-	p = path.Clean(p)
-	// empty path (p == "")
-	if p == "." {
-		return parentFolderId, nil
-	}
-	folders := strings.Split(p, "/")
-	for _, folderName := range folders {
-		// https://developers.google.com/drive/api/v3/search-files
-		q := fmt.Sprintf("name = '%s' and mimeType = 'application/vnd.google-apps.folder' and '%s' in parents", folderName, parentFolderId)
-		files, err := svc.Files.List().Q(q).Spaces("drive").Do()
-		if err != nil {
-			return "", errors.Wrapf(err, "failed to find folder %s inside parent folder %s", folderName, parentFolderId)
-		}
-		if len(files.Files) > 0 {
-			parentFolderId = files.Files[0].Id
-		} else {
-			// https://developers.google.com/drive/api/v3/folder#java
-			folderMetadata := &drive.File{
-				Name:     folderName,
-				MimeType: "application/vnd.google-apps.folder",
-				Parents:  []string{parentFolderId},
-			}
-			folder, err := svc.Files.Create(folderMetadata).Fields("id").Do()
-			if err != nil {
-				return "", errors.Wrapf(err, "failed to create folder %s inside parent folder %s", folderName, parentFolderId)
-			}
-			parentFolderId = folder.Id
-		}
-	}
-	return parentFolderId, nil
-}
-
-func CopyDoc(svcDrive *drive.Service, svcDocs *docs.Service, templateDocId string, folderId string, docName string, replacements map[string]string) (string, error) {
-	var docId string
-
-	// https://developers.google.com/drive/api/v3/search-files
-	q := fmt.Sprintf("name = '%s' and mimeType = 'application/vnd.google-apps.document' and '%s' in parents", docName, folderId)
-	files, err := svcDrive.Files.List().Q(q).Spaces("drive").Do()
-	if err != nil {
-		return "", errors.Wrapf(err, "failed to find doc %s inside parent folder %s", docName, folderId)
-	}
-	if len(files.Files) > 0 {
-		docId = files.Files[0].Id
-	} else {
-		// https://developers.google.com/docs/api/how-tos/documents#copying_an_existing_document
-		copyMetadata := &drive.File{
-			Name:    docName,
-			Parents: []string{folderId},
-		}
-		doc, err := svcDrive.Files.Copy(templateDocId, copyMetadata).Fields("id").Do()
-		if err != nil {
-			return "", errors.Wrapf(err, "failed to copy template doc %s into folder %s", templateDocId, folderId)
-		}
-		docId = doc.Id
-
-		if len(replacements) > 0 {
-			// https://developers.google.com/docs/api/how-tos/merge
-			req := &docs.BatchUpdateDocumentRequest{
-				Requests: make([]*docs.Request, 0, len(replacements)),
-			}
-			for k, v := range replacements {
-				req.Requests = append(req.Requests, &docs.Request{
-					ReplaceAllText: &docs.ReplaceAllTextRequest{
-						ContainsText: &docs.SubstringMatchCriteria{
-							MatchCase: true,
-							Text:      k,
-						},
-						ReplaceText: v,
-					},
-				})
-			}
-			_, err := svcDocs.Documents.BatchUpdate(docId, req).Do()
-			if err != nil {
-				return "", errors.Wrapf(err, "failed to replace template fields in doc %s", docId)
-			}
-		}
-	}
-	return docId, nil
 }
 
 func printJSON(v interface{}) {
@@ -300,42 +197,11 @@ func (d Duration) MarshalJSON() ([]byte, error) {
 	return json.Marshal(d.Duration.String())
 }
 
-type NewsSnippet struct {
-	Content   string `json:"content" csv:"Content"`
-	StartDate Date   `json:"startDate" csv:"Start Date"`
-	EndDate   Date   `json:"endDate" csv:"End Date"`
-}
-
-//func (s *Server) RegisterNewsAPI(m *macaron.Macaron) {
-//	m.Get("/_/news", func(ctx *macaron.Context, c cache.Cache, log *log.Logger) {
-//		key := ctx.Req.URL.Path
-//		out := c.Get(key)
-//		if out == nil {
-//			news, err := s.NextNewsSnippet()
-//			if err != nil {
-//				ctx.Error(http.StatusInternalServerError, err.Error())
-//				return
-//			}
-//			out = news
-//			_ = c.Put(key, out, 60) // cache for 60 seconds
-//		} else {
-//			log.Println(key, "found")
-//		}
-//		ctx.JSON(http.StatusOK, out)
-//	})
-//}
-
-// GET Page
-
 type ConfigType string
 
 const (
 	ConfigTypeQuestion ConfigType = "QuestionConfig"
 )
-
-type IConfigType interface {
-	Type() ConfigType
-}
 
 type QuestionConfig struct {
 	ConfigType            ConfigType `json:"configType" csv:"Config Type"`
@@ -343,12 +209,6 @@ type QuestionConfig struct {
 	StartDate             Date       `json:"startDate" csv:"Start Date"`
 	EndDate               Date       `json:"endDate" csv:"End Date"`
 	Duration              Duration   `json:"duration"  csv:"Duration"`
-}
-
-var _ IConfigType = QuestionConfig{}
-
-func (q QuestionConfig) Type() ConfigType {
-	return q.ConfigType
 }
 
 const (
@@ -361,7 +221,7 @@ func SaveConfig(svcSheets *sheets.Service, configDocId string, cfg QuestionConfi
 		Header: "Config Type",
 		By: func(column []interface{}) (int, error) {
 			for i, v := range column {
-				if v.(string) == string(cfg.Type()) {
+				if v.(string) == string(cfg.ConfigType) {
 					return i, nil
 				}
 			}
