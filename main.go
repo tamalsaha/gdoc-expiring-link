f6c17869ffaf4b319480488a46a84277f7c58078
Tamal Saha <tamal@appscode.com>
2022-05-29 06:33:56 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -6,14 +6,19 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"path"
+	"sort"
 	"strings"
 	"time"
 
+	"github.com/gocarina/gocsv"
+	"google.golang.org/api/sheets/v4"
+
 	"github.com/pkg/errors"
 	_ "gomodules.xyz/gdrive-utils"
-	gdrive_utils "gomodules.xyz/gdrive-utils"
+	gdrive "gomodules.xyz/gdrive-utils"
 	"google.golang.org/api/docs/v1"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
@@ -29,7 +34,28 @@ func handleError(err error, message string) {
 }
 
 func main() {
-	client, err := gdrive_utils.DefaultClient(".")
+	client, err := gdrive.DefaultClient(".")
+	handleError(err, "Error creating YouTube client")
+
+	svcSheets, err := sheets.NewService(context.TODO(), option.WithHTTPClient(client))
+	handleError(err, "Error creating Sheets client")
+
+	configDocId := "1KB_Efi9jQcJ0_tCRF4fSLc6TR7QxaBKg05cKXAwbC9E"
+	qaTemplateDocId := "16Ff6Lum3F6IeyAEy3P5Xy7R8CITIZRjdwnsRwBg9rD4"
+	now := time.Now()
+	cfg := QuestionConfig{
+		ConfigType:            ConfigTypeQuestion,
+		QuestionTemplateDocId: qaTemplateDocId,
+		StartDate:             Date{now},
+		EndDate:               Date{now.Add(3 * 24 * time.Hour)}, // 3 days
+		DurationMinutes:       60,                                // 60 mins
+	}
+	err = SaveConfig(svcSheets, configDocId, cfg)
+	handleError(err, "failed to save config")
+}
+
+func main_() {
+	client, err := gdrive.DefaultClient(".")
 	handleError(err, "Error creating YouTube client")
 
 	svcDrive, err := drive.NewService(context.TODO(), option.WithHTTPClient(client))
@@ -190,4 +216,206 @@ func CopyDoc(svcDrive *drive.Service, svcDocs *docs.Service, templateDocId strin
 func printJSON(v interface{}) {
 	data, _ := json.MarshalIndent(v, "", "  ")
 	fmt.Println(string(data))
+}
+
+const (
+	TimestampFormat = "1/2/2006 15:04:05"
+	DateFormat      = "1/2/2006"
+)
+
+type Timestamp struct {
+	time.Time
+}
+
+// Convert the internal date as CSV string
+func (date *Timestamp) MarshalCSV() (string, error) {
+	return date.Time.Format(TimestampFormat), nil
+}
+
+// Convert the CSV string as internal date
+func (date *Timestamp) UnmarshalCSV(csv string) (err error) {
+	date.Time, err = time.Parse(TimestampFormat, csv)
+	return err
+}
+
+type Date struct {
+	time.Time
+}
+
+// Convert the internal date as CSV string
+func (date *Date) MarshalCSV() (string, error) {
+	return date.Time.Format(DateFormat), nil
+}
+
+// Convert the CSV string as internal date
+func (date *Date) UnmarshalCSV(csv string) (err error) {
+	date.Time, err = time.Parse(DateFormat, csv)
+	return err
+}
+
+type NewsSnippet struct {
+	Content   string `json:"content" csv:"Content"`
+	StartDate Date   `json:"startDate" csv:"Start Date"`
+	EndDate   Date   `json:"endDate" csv:"End Date"`
+}
+
+//func (s *Server) RegisterNewsAPI(m *macaron.Macaron) {
+//	m.Get("/_/news", func(ctx *macaron.Context, c cache.Cache, log *log.Logger) {
+//		key := ctx.Req.URL.Path
+//		out := c.Get(key)
+//		if out == nil {
+//			news, err := s.NextNewsSnippet()
+//			if err != nil {
+//				ctx.Error(http.StatusInternalServerError, err.Error())
+//				return
+//			}
+//			out = news
+//			_ = c.Put(key, out, 60) // cache for 60 seconds
+//		} else {
+//			log.Println(key, "found")
+//		}
+//		ctx.JSON(http.StatusOK, out)
+//	})
+//}
+
+const (
+	NewsSnippetSpreadsheetId = ""
+	NewsSnippetSheet         = ""
+)
+
+func NextNewsSnippet(srvSheets *sheets.Service) (*NewsSnippet, error) {
+	now := time.Now()
+
+	reader, err := gdrive.NewRowReader(srvSheets, NewsSnippetSpreadsheetId, NewsSnippetSheet, &gdrive.Predicate{
+		Header: "End Date",
+		By: func(column []interface{}) (int, error) {
+			for i, v := range column {
+				var d Date
+				err := d.UnmarshalCSV(v.(string))
+				if err != nil {
+					return -1, err
+				}
+				if d.Time.After(now) {
+					return i, nil
+				}
+			}
+			return -1, io.EOF
+		},
+	})
+	if err == io.EOF {
+		return &NewsSnippet{}, nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	snippets := []*NewsSnippet{}
+	if err := gocsv.UnmarshalCSV(reader, &snippets); err != nil { // Load clients from file
+		return nil, err
+	}
+	sort.Slice(snippets, func(i, j int) bool {
+		return snippets[i].EndDate.Before(snippets[j].EndDate.Time)
+	})
+	for i, s := range snippets {
+		if s.EndDate.After(now) {
+			snippets = snippets[i:]
+			break
+		}
+	}
+	if now.After(snippets[0].StartDate.Time) {
+		return snippets[0], nil
+	}
+	return &NewsSnippet{}, nil
+}
+
+// GET Page
+
+type ConfigType string
+
+const (
+	ConfigTypeQuestion ConfigType = "QuestionConfig"
+)
+
+type IConfigType interface {
+	Type() ConfigType
+}
+
+type QuestionConfig struct {
+	ConfigType            ConfigType `json:"configType" csv:"Config Type"`
+	QuestionTemplateDocId string     `json:"questionTemplateDocId" csv:"Question Template Doc Id"`
+	StartDate             Date       `json:"startDate" csv:"Start Date"`
+	EndDate               Date       `json:"endDate" csv:"End Date"`
+	DurationMinutes       int        `json:"durationMinutes"  csv:"Duration Minutes"`
+}
+
+var _ IConfigType = QuestionConfig{}
+
+func (q QuestionConfig) Type() ConfigType {
+	return q.ConfigType
+}
+
+const (
+	ProjectConfigSheet = "config"
+)
+
+func SaveConfig(srvSheets *sheets.Service, configDocId string, cfg IConfigType) error {
+	w := gdrive.NewRowWriter(srvSheets, configDocId, ProjectConfigSheet, &gdrive.Predicate{
+		Header: "Config Type",
+		By: func(column []interface{}) (int, error) {
+			for i, v := range column {
+				if v.(string) == string(cfg.Type()) {
+					return i, nil
+				}
+			}
+			return -1, io.EOF
+		},
+	})
+
+	data := []IConfigType{
+		cfg,
+	}
+	return gocsv.MarshalCSV(data, w)
+}
+
+func GetPage(srvSheets *sheets.Service, configDocId string) {
+	now := time.Now()
+
+	reader, err := gdrive.NewRowReader(srvSheets, configDocId, ProjectConfigSheet, &gdrive.Predicate{
+		Header: "Config Type",
+		By: func(column []interface{}) (int, error) {
+			for i, v := range column {
+				var d Date
+				err := d.UnmarshalCSV(v.(string))
+				if err != nil {
+					return -1, err
+				}
+				if d.Time.After(now) {
+					return i, nil
+				}
+			}
+			return -1, io.EOF
+		},
+	})
+	if err == io.EOF {
+		return &NewsSnippet{}, nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	snippets := []*NewsSnippet{}
+	if err := gocsv.UnmarshalCSV(reader, &snippets); err != nil { // Load clients from file
+		return nil, err
+	}
+	sort.Slice(snippets, func(i, j int) bool {
+		return snippets[i].EndDate.Before(snippets[j].EndDate.Time)
+	})
+	for i, s := range snippets {
+		if s.EndDate.After(now) {
+			snippets = snippets[i:]
+			break
+		}
+	}
+	if now.After(snippets[0].StartDate.Time) {
+		return snippets[0], nil
+	}
+	return &NewsSnippet{}, nil
 }
