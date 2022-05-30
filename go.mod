f8b4753960e25f905487e9a80a4f721899bd6cae
Tamal Saha <tamal@appscode.com>
2022-05-29 23:04:36 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/tamalsaha/gdoc-expiring-link
 go 1.18
 
 require (
-	github.com/flower-corp/lotusdb v1.0.0
 	github.com/gocarina/gocsv v0.0.0-20220520193141-bb9bebb918c3
 	github.com/madflojo/tasks v1.0.3
 	github.com/pkg/errors v0.9.1
+	github.com/rs/xid v1.3.0
+	github.com/syndtr/goleveldb v1.0.0
 	gomodules.xyz/encoding v0.0.5
 	gomodules.xyz/gdrive-utils v0.0.7
 	google.golang.org/api v0.81.0
@@ -16,9 +17,8 @@ require (
 	cloud.google.com/go/compute v1.6.1 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
-	github.com/rs/xid v1.3.0 // indirect
-	go.etcd.io/bbolt v1.3.6 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
 	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
--- scheduler/main.go
+++ scheduler/main.go
@@ -2,11 +2,91 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	"time"
 
 	"github.com/madflojo/tasks"
+	"github.com/pkg/errors"
+	"github.com/rs/xid"
+	"github.com/syndtr/goleveldb/leveldb"
 )
+
+type Scheduler struct {
+	db *leveldb.DB
+	s  *tasks.Scheduler
+}
+
+func NewScheduler() (*Scheduler, error) {
+	path, _ := ioutil.TempDir("", "tasks")
+	db, err := leveldb.OpenFile(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &Scheduler{
+		s:  tasks.New(),
+		db: db,
+	}, nil
+}
+
+func (s *Scheduler) Close() error {
+	s.s.Stop()
+	return s.db.Close()
+}
+
+func (s *Scheduler) Cleanup(fn func(interface{}) error) error {
+	iter := s.db.NewIterator(nil, nil)
+	for iter.Next() {
+		// Remember that the contents of the returned slice should not be modified, and
+		// only valid until the next call to Next.
+		key := iter.Key()
+		args := iter.Value()
+
+		if t, _ := s.s.Lookup(string(key)); t != nil {
+			continue
+		}
+
+		if err := fn(args); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "failed to execute cleanup function for task id %s, args %s, err: %v", string(key), string(args), err)
+		}
+		if err := s.db.Delete(key, nil); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "failed to delete key %s, args %s, err: %v", string(key), string(args), err)
+		}
+	}
+	iter.Release()
+	return iter.Error()
+}
+
+func (s *Scheduler) Schedule(t time.Time, fn func([]byte) error, args []byte) error {
+	for {
+		id := xid.New()
+		err := s.s.AddWithID(id.String(), &tasks.Task{
+			Interval: time.Until(t),
+			RunOnce:  true,
+			TaskFunc: func() error {
+				if err := fn(args); err != nil {
+					return err
+				}
+				return s.db.Delete(id.Bytes(), nil)
+			},
+			ErrFunc: func(e error) {
+				_ = s.db.Delete(id.Bytes(), nil)
+				_, _ = fmt.Fprintf(os.Stderr, "an error occurred when executing task %s - %v", id, e)
+			},
+		})
+		if err == tasks.ErrIDInUse {
+			continue
+		} else if err != nil {
+			return errors.Wrapf(err, "failed to schedule task")
+		}
+		if err = s.db.Put(id.Bytes(), args, nil); err != nil {
+			return err
+		}
+		break
+	}
+	return nil
+}
 
 // https://github.com/madflojo/tasks/blob/main/tasks_test.go
 func main() {
